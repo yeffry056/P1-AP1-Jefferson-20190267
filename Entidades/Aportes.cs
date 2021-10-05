bf7195acdc57865f44e9203ebec98dd6e9d1c5f5
Jefferson Javier Vasquez <jefferson_javier@ucne.edu.do>
2021-10-04 22:06:10 -0400

    Creacion de la entidad con sus propiedades

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Jefferson_20190267.Entidades
+{
+    public class Aportes
+    {
+       [Key]
+        public int AporteId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Nombre { get; set; }
+        public string Concepto { get; set; }
+        public int Monto { get; set; }
+
+    }
+}
